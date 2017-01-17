121649d48e32d2ff9fee109e99242aa3e23468a5
misalabs <arensiatik@hotmail.com>
2017-01-17 00:17:33 +0000

    Update to Django 1.8

--- ajax_select/views.py
+++ ajax_select/views.py
@@ -1,9 +1,9 @@
+import json as simplejson
 
 from ajax_select import get_lookup
 from django.contrib.admin import site
 from django.db import models
 from django.http import HttpResponse
-from django.utils import simplejson
 
 
 def ajax_lookup(request,channel):
